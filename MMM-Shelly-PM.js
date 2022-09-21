7858f8ba537944f7b6ad9a09e27276c568fab9a4
Stefan Jacobs <stefan.jacobs@deutschebahn.com>
2022-09-21 22:23:16 +0200

    Added daily consumption data from cloud, first draft

--- MMM-Shelly-PM.js
+++ MMM-Shelly-PM.js
@@ -12,7 +12,8 @@ Module.register("MMM-Shelly-PM",{
 	ShellyPDData: {
 		tmp: "--",
 		apower: "--",
-		updated: "--"
+		updated: "--",
+		total: "--"
 	},
 	getStyles: function () {
 		return ["MMM-Shelly-PM.css"]; //, "font-awesome.css"];
@@ -24,8 +25,15 @@ Module.register("MMM-Shelly-PM",{
 		setInterval(function() {
 			self.sendSocketNotification("GetShelly", self.config.ShellyApiPath);
 			self.updateDom();
-		}, this.config.RefreshInterval);
+		}, this.config.RefreshIntervalLAN);
 
+		setInterval(function() {
+			self.sendSocketNotification("GetShellyCloud", self.config.CloudServerPath);
+			self.updateDom();
+		}, this.config.RefreshIntervalCloud);
+		// update initial data
+		// self.sendSocketNotification("GetShelly");
+		self.sendSocketNotification("GetShellyCloud");
 	},
 	socketNotificationReceived: function (notification, payload) {
 		if (notification = "ShellyPDData"){
@@ -34,11 +42,15 @@ Module.register("MMM-Shelly-PM",{
 			this.ShellyPDData.apower = payload.apower
 			this.ShellyPDData.updated = payload.updated
 		}
+		if (notification = "ShellyCloudData") {
+			this.ShellyPDData.total = payload.total
+		}
 	},
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		var apower = this.translate("APOWER")
+		var apower = this.translate("APOWER");
+		var totalday = this.translate("TOTALDAY");
 		if (this.config.negativeDisplay && this.ShellyPDData.apower > 0.4) {
 			var apower_unit = this.translate("APOWER_UNIT", {"apower": -this.ShellyPDData.apower});
 			var csstype = "generation"
@@ -46,13 +58,24 @@ Module.register("MMM-Shelly-PM",{
 			var apower_unit = this.translate("APOWER_UNIT", {"apower": this.ShellyPDData.apower});
 			var csstype = "consumption"
 		}
+
+		if (this.config.negativeDisplay && this.ShellyPDData.total > 0.4) {
+			var total_unit = this.translate("APOWER_UNIT", {"apower": -this.ShellyPDData.total})
+			var totalcsstype = "generation"
+		} else {
+			var total_unit = this.translate("APOWER_UNIT", {"apower": this.ShellyPDData.total})
+			var totalcsstype = "consumption"
+		}
+
 		var tmp = this.translate("TEMPERATURE", {"tmp": this.ShellyPDData.tmp});
 		var updated = this.translate("UPDATED", {"upd": this.ShellyPDData.updated})
 		ihtml =  "<div class='container'>"
 		if (this.config.horizontalView) {
 			ihtml += "  <div class='right " + csstype + "'><sup>" + apower + "</sup> " + apower_unit + "</div>"
+			ihtml += "  <div class='newline right " + totalcsstype + "'><sup>" + totalday + "</sup> " + total_unit + "</div>"
 		} else {
 			ihtml += "  <div class='newline " + csstype + "'><sup>" + apower + "</sup>" + apower_unit + "</div>"
+			ihtml += "  <div class='newline " + totalcsstype + "'><sup>" + totalday + "</sup> " + total_unit + "</div>"
 		}
 		if (this.config.displayUpdated){
 			ihtml += "  <p class='bottom'>" + tmp + " ℃ " + updated + "</p>"
--- node_helper.js
+++ node_helper.js
@@ -4,16 +4,23 @@ const request = require('request');
 
 module.exports = NodeHelper.create({
 
+	oldData: {
+        tmp: "--",
+		apower: "--",
+		updated: "--",
+		total: "--"
+	},
+
 	start: function() {
 
 	},
 
-	// Frontend module pings the node helper to fetch data from Shelly HT
+	// Frontend module pings the node helper to fetch data from Shelly PM
 	socketNotificationReceived: function (notification, payload) {
 		if (notification == "GetShelly"){
-			//Parameters: notification can be anything (not used), payload must be the URL of the Shelly HT status api
+			//Parameters: notification can be anything (not used), payload must be the URL of the Shelly PM status api
 			self = this;
-			request(payload, {json: true }, (err, res, body) => {
+			request(payload, {json: true, forever: true, timeout: 2500 }, (err, res, body) => {
 				if (err) { return console.log(err); }
 
 				currentdate = new Date();
@@ -24,12 +31,36 @@ module.exports = NodeHelper.create({
 				payload= {
 					apower: body['switch:0'].apower,
 					tmp: body['switch:0'].temperature.tC,
-					updated: printed_date
+					updated: printed_date,
+					total: self.oldData.total
 				}
 				//console.log("Sending Shelly data to FE module", payload);
+				self.oldData.apower = payload.apower;
+				self.oldData.tmp = payload.tmp;
+				self.oldData.updated = payload.updated;
+				self.oldData.total = payload.total;
 
 				self.sendSocketNotification('ShellyPDData', payload)
 			});
 		}
+
+		if (notification == "GetShellyCloud") {
+			self = this
+
+			payload= {
+				apower: self.oldData.apower,
+				tmp: self.oldData.tmp,
+				updated: self.oldData.updated,
+				total: 1000
+			}
+			self.oldData.apower = payload.apower;
+			self.oldData.tmp = payload.tmp;
+			self.oldData.updated = payload.updated;
+			self.oldData.total = payload.total;
+			self.sendSocketNotification('ShellyCloudData', payload)
+
+		}
+
+
 	}
 });
