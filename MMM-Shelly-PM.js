b7fca329e78ffef1b8812432cea62d49e6eb2aa7
Stefan Jacobs <stefan.jacobs@deutschebahn.com>
2022-10-10 15:43:28 +0200

    Removed EnergyMonitor Notification

--- MMM-Shelly-PM.js
+++ MMM-Shelly-PM.js
@@ -52,7 +52,6 @@ Module.register("MMM-Shelly-PM",{
 			this.ShellyPDData.tmp = payload.tmp
 			this.ShellyPDData.apower = payload.apower
 			this.ShellyPDData.updated = payload.updated
-			this.sendNotification("MMM-EnergyMonitor_SOLAR_POWER_UPDATE", payload.apower);
 		}
 		if (notification == "ShellyCloudData") {
 			this.ShellyPDData.total = payload.total
